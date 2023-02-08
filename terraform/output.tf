b10e9844251a20b0b8ded6c5dbbc54249abe2b41
ninjaka <anpod07@gmail.com>
2023-02-08 13:44:46 +0200

    auto-commit

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,7 +2,7 @@ output "my_tf_ec2_elastic_ip" {
   value = aws_eip.TF_elastic_ip.public_ip
 }
 
-#output "my_tf_rds_address" {
-#  value = aws_db_instance.my_test_mysql.address
-#}
+output "my_tf_rds_address" {
+  value = aws_db_instance.my_test_mysql.address
+}
 
